629c0b60600c22ae2aaffc80555334aafbcba05f
morgan krischer <morgan.krischer@gmail.com>
2018-05-16 19:26:30 +0100

    Fake Airbnb Show page cards empty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'London Dump',
+  address: 'Under London Bridge',
+  description: 'Great view but a bit draughty',
+  price_per_night: 0,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Le Wagon',
+  address: '138 Kingsland Road',
+  description: 'Spacious and light',
+  price_per_night: 100,
+  number_of_guests: 32
+)
+
+Flat.create!(
+  name: 'Teapot for drugged up mouse',
+  address: 'Wonderland',
+  description: 'Cosy and tealightful',
+  price_per_night: 10,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Westminster Palace',
+  address: 'Westminster',
+  description: 'Quaint little place',
+  price_per_night: 10_000,
+  number_of_guests: 125
+)
+
+Flat.create!(
+  name: 'Lost dreams',
+  address: 'Saffron Hill',
+  description: 'Lovely duplex in the centre of London, perfect to make plans on the comet',
+  price_per_night: 90,
+  number_of_guests: 4
+)
